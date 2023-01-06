3d1cb9286554d944406489b898909d40b3feb623
Warren Hong <warren@Warrens-MacBook-Pro.local>
2023-01-05 16:36:13 -0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+         HELLO
         </p>
       </header>
       <div style={receivedText ? { fontSize: "48px", color: "red" } : {}}>
