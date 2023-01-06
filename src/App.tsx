36b7c08f122aa326434a63c132e32e75afc3ab4e
Warren Hong <warren@Warrens-MacBook-Pro.local>
2023-01-05 21:12:12 -0800

    work please

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>PLEASE WORK iii</p>
+        <p>PLEASE WORK PLEASE</p>
         <div
           className="text"
           style={
