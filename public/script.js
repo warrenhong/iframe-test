61aed69e4d5f62a531e26a1586993ecc3dbe31c6
Warren Hong <warren@Warrens-MacBook-Pro.local>
2023-01-06 10:32:31 -0800

    attempt

--- public/script.js
+++ public/script.js
@@ -3,7 +3,6 @@ const elem = document.getElementById("received");
 window.addEventListener("message", (event) => {
   const { data } = event;
 
-
   if (data.eventSourceKey === "lolol") {
     elem.innerText = data.payload.dede;
   }
