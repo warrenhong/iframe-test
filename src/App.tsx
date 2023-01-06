83a2a17e4338054f0bb910003a6aa63c0a41a6de
Warren Hong <warren@Warrens-MacBook-Pro.local>
2023-01-05 21:20:56 -0800

    work pleasEEEEEe

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [receivedText, setReceivedText] = useState<string>("hey hey");
-
+  const onMessageReceived = useCallback((event: any) => {
+    const { data } = event;
+    console.log(data);
+    console.log(receivedText)
+    if (data.eventSourceKey === "lolol") {
+      setReceivedText(JSON.stringify(data.eventSourceKey));
+    }
+  },[receivedText]);
   useEffect(() => {
-    const onMessageReceived = async (event: any) => {
-      const { data } = event;
-      console.log(data);
-      if (data.eventSourceKey) {
-        setReceivedText(JSON.stringify(data.eventSourceKey));
-      }
-    };
     window.addEventListener("message", onMessageReceived);
 
     return () => {
       window.removeEventListener("message", onMessageReceived);
     };
-  }, []);
+  }, [onMessageReceived]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>PLEASE WORK DEWEWDWEDWE</p>
+        <p>PLEASE WORK PLEASE WORK PLEASE WORK</p>
         <div
           className="text"
           style={
