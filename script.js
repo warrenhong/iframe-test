4663afc1b3ab2cf966a476c61d317a5da1527310
Warren Hong <warren@Warrens-MacBook-Pro.local>
2023-01-05 22:21:13 -0800

    Updates

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("dede")
+alert("dede");
+
+const elem = document.getElementById("received");
+
+window.addEventListener("message", (event) => {
+  const { data } = event;
+  console.log(data);
+
+  if (data.eventSourceKey === "lolol") {
+    elem.innerText = data.eventSourceKey;
+  }
+});
