531f49f83d378799837e7123418ae063a8640db1
Warren Hong <warren@Warrens-MacBook-Pro.local>
2023-01-06 11:34:23 -0800

    attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,19 @@
-import React, { useCallback, useEffect } from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const calledOnce = React.useRef(false);
-
-  const onMessageReceived = useCallback((event: any) => {
-    const { data } = event;
-    console.log(data);
-
-    if (data.eventSourceKey === "lolol") {
-      // setReceivedText(JSON.stringify(data.eventSourceKey));
-      calledOnce.current = true;
-    }
-  }, []);
-
-  useEffect(() => {
-    if (calledOnce.current) {
-      return;
-    }
-
-    window.addEventListener("message", onMessageReceived);
-
-    return () => {
-      window.removeEventListener("message", onMessageReceived);
-    };
-  }, [onMessageReceived]);
-
   return (
     <div className="App">
       <header className="App-header">DEMO</header>
       <button
         onClick={() => {
-          window.postMessage(
-            {
-              eventSourceKey: "nyc",
-              data: { variable: "HEY I'M FROM THE IFRAME" },
-            },
-            "*"
-          );
+          const payload = {
+            eventSourceKey: "nyc",
+            data: { variable: "HEY I'M FROM THE IFRAME" },
+          };
+          
+          window.parent.postMessage(payload, "*");
+          window.postMessage(payload, "*");
         }}
       >
         SEND TO AIRKIT
