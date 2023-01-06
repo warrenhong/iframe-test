b88ca4328fac675f4990842e773255aa18529cea
Warren Hong <warren@Warrens-MacBook-Pro.local>
2023-01-06 11:46:55 -0800

    attempt

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">DEMO</header>
+      <header className="App-header">DEMO 2</header>
       <button
         onClick={() => {
           const payload = {
