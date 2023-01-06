51aa8c2184210bf2e1f7bd83b65b0816cfcb3c71
Warren Hong <warren@Warrens-MacBook-Pro.local>
2023-01-05 17:17:01 -0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [receivedText, setReceivedText] = useState("");
 
+  const onMessageReceived = useCallback((event: any) => {
+    const { data, eventSourceKey } = event.data;
+    console.log(event);
+    setReceivedText(data.dede);
+  }, []);
   useEffect(() => {
-    const onMessageReceived = (event: any) => {
-      const { data, eventSourceKey } = event.data;
-      console.log(event)
-      setReceivedText(data.dede);
-    };
     window.addEventListener("message", onMessageReceived);
 
     return () => {
       window.removeEventListener("message", onMessageReceived);
     };
-  }, []);
+  }, [onMessageReceived]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         HELLO 3
-        </p>
+        <p>HELLO 223123</p>
       </header>
       <div style={receivedText ? { fontSize: "48px", color: "red" } : {}}>
         {receivedText}
