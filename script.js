e3da6959e37594cbe95d6b94a21974d914f52a4d
Warren Hong <warren@Warrens-MacBook-Pro.local>
2023-01-06 11:34:34 -0800

    Updates

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ window.addEventListener("message", (event) => {
     elem.innerText = data.payload.dede;
   }
 });
+
+
+
+// const calledOnce = React.useRef(false);
+
+// const onMessageReceived = useCallback((event: any) => {
+//   const { data } = event;
+//   console.log(data);
+
+//   if (data.eventSourceKey === "lolol") {
+//     // setReceivedText(JSON.stringify(data.eventSourceKey));
+//     calledOnce.current = true;
+//   }
+// }, []);
+
+// useEffect(() => {
+//   if (calledOnce.current) {
+//     return;
+//   }
+
+//   window.addEventListener("message", onMessageReceived);
+
+//   return () => {
+//     window.removeEventListener("message", onMessageReceived);
+//   };
+// }, [onMessageReceived]);
