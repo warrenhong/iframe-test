1b683bbf4ac9f65c8f663251ed8549d325d7eafe
Warren Hong <warren@Warrens-MacBook-Pro.local>
2023-01-11 09:47:10 -0800

    attempt

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <header className="App-header">war</header>
       <header className="App-header">iframe Bi-Directional Demo</header>
       <button
       className="button"
